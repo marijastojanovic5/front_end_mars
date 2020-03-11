647fc2301852ed24bb4324ed1b88714612c7db0f
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-03-11 12:09:09 -0400

    rendering pics on page

--- src/App.js
+++ src/App.js
@@ -2,22 +2,33 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom'
 import NavBar from './components/NavBar'
 import About from './components/About'
-
-
 import './App.css';
+import MarsRover from './components/MarsRover';
 
-function App() {
+class App extends React.Component {
+   state={
+      marsRoverArray: []
+    }
+  
+  componentDidMount(){
+    fetch("http://localhost:4000/mars_cards")
+    .then(res=>res.json())
+    .then(data=>console.log(data))
+    }
+    render(){
   return (
+    
     <div className="App">
      
         <NavBar />
         <Route exact path="/about" component={About}/>
-        
+        <Route exact path="/marsrover" component={MarsRover}/>
         
        
    
     </div>
-  );
+  )
+  }
 }
 
 export default App;
