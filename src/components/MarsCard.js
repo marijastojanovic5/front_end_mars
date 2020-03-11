149a03ffe4c6753453d563dcc5e0dc71d90cf001
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-03-11 15:33:47 -0400

    rover pics rendered w launch and landing date

--- src/components/MarsCard.js
+++ src/components/MarsCard.js
@@ -1,2 +1,25 @@
 import React from 'react'
-const
+
+const MarsCard=(props)=>{
+    console.log(props)
+    
+    return(
+         
+        <div class="col mb-4"><div className="card">
+         <h6 className="card-title">Picture taken with Mars Rover</h6>
+        <img  src={props.card.image} className="card-img-top" alt="mars"/>
+         <div className="card-body">
+         <h5 className="card-title">{props.card.name}</h5>
+         <p className="card-text">Launch date: {props.card.launch_date}</p>
+         <p className="card-text">Landing date: {props.card.landing_date}</p>
+         <p className="card-text">Status: {props.card.status}</p>
+         </div>
+        </div>
+        </div>
+       
+       
+       
+    )
+}
+
+export default MarsCard
--- src/components/MarsRover.js
+++ src/components/MarsRover.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import MarsCard from './MarsCard'
 
 const MarsRover=(props)=>{
-    
+   
     return(
-        <div>
+        <div class="row row-cols-1 row-cols-md-4">
             {props.marsRoverArray.map(card=> 
+            
             <MarsCard
             key= {card.id}
             card= {card}
